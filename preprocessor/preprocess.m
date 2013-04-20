67f6832d8688aa31f6d4b73dd494ca09d890a778
Kent Chen <chkche1@gmail.com>
2013-04-20 02:05:34 -0400

    Edited preprocess so that it returns pause length distribution

--- preprocessor/preprocess.m
+++ preprocessor/preprocess.m
@@ -1,40 +1,45 @@
-function [ seg, num_pauses] = preprocess(wavFileName, threshold)
+function [ seg, bins] = preprocess(wavFileName, threshold)
 %PREPROCESS Summary of this function goes here
 %   Detailed explanation goes here
 %
 % ARGUMENTS:
-% - segs: a cell array of audio segments
-% - rate: sampling freq. of the audio signal
-% - threshold: the threshold value (in seconds)
+% - wavFileName - wave file
+% - threshold - the cut value length for all segments
 %
 % RETURNS:
 % - seg: a cell array with audio segments all longer than threshold
-%
+% - bins: pause length distribution
 % 
 % EXECUTION EXAMPLE:
 %
-% seg = removeShortUtterance(segs, 44100, 2)
+% seg = preprocess('example.wav', 2)
 %
+narginchk(2,2);
 [segments, fs, Limits] = detectVoiced(wavFileName);
-numRows = size(segments, 2); num_pauses = 0;
-pause_threshold = 0.25*fs;
-pause_upper_bound = 5*fs;
+numRows = size(segments, 2);
+
 for i=1:numRows
     leng = size(segments{i},1)
     if leng/fs < threshold
         segments{i} = [];
     end
 end
-
+bins = zeros(1, 100);
 for i=1:numRows
     if(isempty(segments{i}))
-        continue
+        continue;
     end
     if(i<numRows)
-        dif = Limits(i+1,2) - Limits(i,1)
-        if(dif>=pause_threshold && dif<=pause_upper_bound)
-            num_pauses = num_pauses+1
+        % dif is in msec
+        dif = (Limits(i+1,2) - Limits(i,1)) / fs * 1000;
+        bucket_no = dif/int32(100);
+        if(bucket_no==0)
+            continue;
         end
+        if(bucket_no>100)
+            bucket_no = 100;
+        end
+        bins(bucket_no) = bins(bucket_no)+1;
     end
 end
 
